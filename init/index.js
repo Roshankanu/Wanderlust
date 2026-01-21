fb1891aa9787cd38579d0ea478d496091dbb826f
Rohit <Roshankr2223@gmail.com>
2026-01-21 19:34:33 +0545

    Fix listings route

--- init/index.js
+++ init/index.js
@@ -24,4 +24,12 @@ const initDB=async ()=>{
     console.log("Data was initialised!!!!!");
     
 }
+const seedDB = async () => {
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log("Database seeded");
+};
+seedDB().then(() => {
+  mongoose.connection.close();
+});
 initDB();
